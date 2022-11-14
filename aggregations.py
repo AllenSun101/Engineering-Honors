ccffdac4b036c70078c44f031c90a3985d1cab7d
Riley Hebert <99193191+UnthinkableShock@users.noreply.github.com>
2022-11-14 12:46:21 -0600

    Changed format of docstrings
    Changed format of the doc-strings for functions in the .py files to a format that VS code recognizes, so it will show up when hovering your mouse over the function name when it is being called.

--- aggregations.py
+++ aggregations.py
@@ -1,6 +1,12 @@
 import pandas as pd
 
 def getCount(dataFrame, cName: str):
+    """
+    @param dataFrame - the dataframe to pull from
+    @param cName - the column to pull data from
+
+    @return dataframe constructed from given dataframe and column name
+    """
     col1 = dataFrame[cName].value_counts().index
     col2 = list(dataFrame[cName].value_counts())
     return pd.DataFrame({cName: col1, "Counts": col2})
--- data_clean.py
+++ data_clean.py
@@ -2,24 +2,26 @@ import pandas as pd
 import numpy as np
 
 
-"""
-@param file_name - name of workbook
 
-@return bundled dataframe of all sheets in workbook
-"""
 def open_file(file_name):
+    """
+    @param file_name - name of workbook
+
+    @return bundled dataframe of all sheets in workbook
+    """
     return pd.ExcelFile(file_name)
 
-"""
-@param file - name of workbook
-@param a_start - column to start on (inclusive)
-@param a_end - column to end on (non-inclusive)
-@param r_start - row to start on (inclusive)
-@param r_end - row to end on (non-inclusive)
 
-@return dict of dataframes for each attended (a) and registered (r) dataset (key=(sheet_name, a/r), value=dataframe)
-"""
 def data_clean(file: str, a_start: int, a_end: int, r_start: int, r_end: int) -> dict:
+    """
+    @param file - name of workbook
+    @param a_start - column to start on (inclusive)
+    @param a_end - column to end on (non-inclusive)
+    @param r_start - row to start on (inclusive)
+    @param r_end - row to end on (non-inclusive)
+
+    @return dict of dataframes for each attended (a) and registered (r) dataset (key=(sheet_name, a/r), value=dataframe)
+    """
     sheets = dict()
     for sheet in file.sheet_names:
         whole_sheet = pd.read_excel(file, sheet)
@@ -34,12 +36,13 @@ def data_clean(file: str, a_start: int, a_end: int, r_start: int, r_end: int) ->
         sheets[(sheet, "r")] = registered
     return sheets
 
-"""
-@param file_name - name of workbook
 
-@return bundled dataframe of all sheets in workbook
-"""
 def create_csvs(sheets):
+    """
+    @param file_name - name of workbook
+
+    @return bundled dataframe of all sheets in workbook
+    """
     for ws in sheets:
         if ws[1] == "a":
             sheets[ws].to_csv("data/" + ws[0] + "_a.csv", index=False)
